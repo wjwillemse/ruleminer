e1047332672d6ddaca23da861a66f09b20367485
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2022-01-24 12:31:42 +0100

    pyparsing>=3.0.0 added

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'pandas', 'pyparsing']
+requirements = ['Click>=7.0', 'pandas', 'pyparsing>=3.0.0']
 
 test_requirements = [ ]
 
