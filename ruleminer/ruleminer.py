220b809082ecd88a052c5dad49abe932705651bd
Willem Jan Willemse <w.j.willemse@freedom.nl>
2024-10-06 15:33:52 +0200

    first attempt to generate test cases

--- ruleminer/ruleminer.py
+++ ruleminer/ruleminer.py
@@ -5,6 +5,7 @@ import itertools
 import re
 import numpy as np
 import pandas as pd
+import constraint
 
 from .parser import (
     dataframe_index,
@@ -12,6 +13,7 @@ from .parser import (
     dataframe_lengths,
     pandas_column,
     rule_expression,
+    python_function,
 )
 
 from .utils import generate_substitutions
@@ -1283,6 +1285,113 @@ class RuleMiner:
                 )
             return "(" + res + ")"
 
+    def get_testcases(self):
+
+        assert self.rules is not None, "Unable to generate test cases, no rules defined."
+
+        functions = {}
+        # column names are mapped to internal names because they can contains 
+        # spaces and other symbols
+        col2args = dict()
+        for idx in self.rules.index:
+            expression = self.rules.loc[idx, RULE_DEF]
+            regex_condition = re.compile(r"if(.*)then(.*)", re.IGNORECASE)
+            rule = regex_condition.search(expression)
+            func_rule_def = {
+                'X': {
+                    'expr': rule.group(1).strip(),
+                },
+                'Y': {
+                    'expr': rule.group(2).strip(),
+                },
+                'not_Y': {
+                    'expr': "not "+rule.group(2).strip(),
+                },
+            }
+            for part in func_rule_def.keys():
+                if func_rule_def[part]['expr'] != '()':
+                    def_body = func_rule_def[part]['expr']
+                    def_name = "_rule_"+str(idx)+"_"+part
+                    columns = list(set([item[2:-2] for item in re.findall(r'{".*?"}', def_body)]))
+                    for col in columns:
+                        if col not in col2args.keys():
+                            col2args[col] = "var_"+str(len(col2args.keys()))
+                    def_args = [col2args[arg] for arg in columns]
+                    for key, value in col2args.items():
+                        def_body = def_body.replace(key, value)
+                    def_code = (
+                        "def "
+                        + def_name
+                        + "("+", ".join(def_args)+"):\n"
+                        + "    return " + def_body.replace('{"', '').replace('"}', '')
+                    )
+                    func_rule_def[part]['def_name'] = def_name
+                    func_rule_def[part]['def_args'] = def_args
+                    exec(def_code)
+                    logging.debug('Executed:\n"'+str(def_code)+'\n"')
+                functions[idx] = func_rule_def
+        args2col = {value: key for key, value in col2args.items()}
+        testcases = pd.DataFrame()
+
+        # testcase that satisfies all rules
+        problem = constraint.Problem()
+        problem.addConstraint(constraint.AllDifferentConstraint())
+        for func_param in col2args.values():
+            logging.debug("Added variable: "+str(func_param))
+            problem.addVariable(func_param, range(0, 10))
+
+        for idx in self.rules.index:
+            expression = self.rules.loc[idx, RULE_DEF]
+            for part in ['X', 'Y']:
+                if 'def_name' in functions[idx][part].keys():
+                    def_name = functions[idx][part]['def_name']
+                    def_args = functions[idx][part]['def_args']
+                    logging.debug("Added constraint: "+str((def_name, def_args)))
+                    problem.addConstraint(eval(def_name), def_args)
+        solutions = problem.getSolutions()
+        if len(solutions) > 0:
+            testcases = pd.concat(
+                [   
+                    testcases, 
+                    pd.DataFrame(
+                        columns=['result']+list(args2col[col] for col in solutions[0].keys()), 
+                        data=[['all satisfied']+list(solutions[0].values())],
+                    ),
+                ], ignore_index=True)
+        else:
+            logging.error("No solution found that satisfy all rules")
+
+        # testcases that satisfy all but one rule
+        for idx2 in self.rules.index:
+            rule_id = self.rules.loc[idx2, RULE_ID]
+            problem = constraint.Problem()
+            problem.addConstraint(constraint.AllDifferentConstraint())
+            for func_param in col2args.values():
+                logging.debug("Added variable: "+str(func_param))
+                problem.addVariable(func_param, range(0, 10))
+            for idx in self.rules.index:
+                expression = self.rules.loc[idx, RULE_DEF]
+                parts = ['X', 'Y' if idx != idx2 else 'not_Y']
+                for part in parts:
+                    if 'def_name' in functions[idx][part].keys():
+                        def_name = functions[idx][part]['def_name']
+                        def_args = functions[idx][part]['def_args']
+                        logging.debug("Added constraint: "+str((def_name, def_args)))
+                        problem.addConstraint(eval(def_name), def_args)
+            solutions = problem.getSolutions()
+            if len(solutions) > 0:
+                testcases = pd.concat(
+                    [   
+                        testcases, 
+                        pd.DataFrame(
+                            columns=['result']+list(args2col[col] for col in solutions[0].keys()), 
+                            data=[['all satisfied, except '+str(rule_id)]+list(solutions[0].values())],
+                        ),
+                    ], ignore_index=True)
+            else:
+                logging.error("No solution found that satisfy all rules except "+str(rule_id))
+
+        return testcases
 
 def flatten_and_sort(expression: str = ""):
     """
